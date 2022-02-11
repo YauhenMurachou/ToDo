7b344272cec5c14648852ba46fe1db8059d9b5c4
Dmitry Smyslov <smyslov-d@mail.ru>
2022-02-11 12:46:15 +0300

    delete at file ddsjbsdj

--- src/test.js
+++ src/test.js
@@ -1,6 +1,2 @@
 console.log('jkdhasfkjabfbjkas');
 console.log('jkdhasfkjabfbjkas');
-console.log('jkdhasfkjabfbjkas');
-console.log('jkdhasfkjabfbjkas');
-console.log('jkdhasfkjabfbjkas');
-console.log('jkdhasfkjabfbjkas');
