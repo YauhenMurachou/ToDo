ef77d78640e2e754230ab9f60228faf2b2b55a34
Dmitry Smyslov <smyslov-d@mail.ru>
2022-02-11 12:36:04 +0300

    Refactoring test.js

--- src/test.js
+++ src/test.js
@@ -1,1 +1,6 @@
 console.log('jkdhasfkjabfbjkas');
+console.log('jkdhasfkjabfbjkas');
+console.log('jkdhasfkjabfbjkas');
+console.log('jkdhasfkjabfbjkas');
+console.log('jkdhasfkjabfbjkas');
+console.log('jkdhasfkjabfbjkas');
