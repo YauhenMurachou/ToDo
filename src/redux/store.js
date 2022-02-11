108df1e412c4f881c0e3dec381076641efdb99f5
YauhenMurachou <emurach1992@gmail.com>
2022-02-11 12:11:04 +0300

    Delete files

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,6 @@ import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import rootReducer from "./reducers";
 
-
 const store = createStore(
 	rootReducer,
 	composeWithDevTools(
